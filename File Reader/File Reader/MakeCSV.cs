e6128ee57862defbdc53247a21ffec33398a6901
AlexanderEnriquezCCC <44684320+AlexanderEnriquezCCC@users.noreply.github.com>
2020-10-02 14:24:31 -0500

    Add files via upload

---
+++ File Reader/File Reader/MakeCSV.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace File_Reader
+{
+    //find way to add 2 CSV file values
+    //print values int a new csv file
+    class MakeCSV
+    {
+        public void getData()
+        {
+            var files = Directory.EnumerateFiles("path", "*.csv");
+        }
+    }
+}
---
+++ File Reader/File Reader/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace File_Reader
+{
+    // going to make pseudo-code cuz after looking at how I can do this for nearly 5 days I still don't know how to do it.
+    /// <summary>
+    /// Program starts
+    /// starts to read CSV file from bin > debug and pull number values, put numbers into an array or list
+    /// If it pulls more than 1 CSV file, adds arrays/lists for each value there is.
+    /// Pastes onto a CSV file created from in running code.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
